000a626557c2ea9bde689bdb9554c1512bfa3ffe
Laxmi Bhandari <laxmibhandari2345@gmail.com>
2022-09-08 16:21:36 +0545

    new data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
-import './App.css';
+/*import './App.css';
 
 function App() {
   return (
     <div className="App">
       <p>Hello,I am laxmi</p>
+    </div>
+  );
+}
+
+export default App;*/
+import './App.css';
+import Todo from './components/Todo'
+
+function App() {
+  return (
+    <div className="App">
+      <Todo />
     </div>
   );
 }
